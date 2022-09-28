2ced0a94cbba05a111e0d0dc17204e155b64e7d4
Maan <manvendrarana@hotmail.com>
2022-09-28 15:36:44 -0400

    Added Geolocation via opencagedata API

--- script.js
+++ script.js
@@ -40,6 +40,68 @@ let weather = {
 	},
 };
 
+let geocode = {
+	reverseGeocode: function (latitude, longitude) {
+		var api_key = "5180226048c94d3e8c7b04f4ddad21a9";
+
+		var api_url = "https://api.opencagedata.com/geocode/v1/json";
+
+		var request_url =
+			api_url +
+			"?" +
+			"key=" +
+			api_key +
+			"&q=" +
+			encodeURIComponent(latitude + "," + longitude) +
+			"&pretty=1" +
+			"&no_annotations=1";
+
+		// see full list of required and optional parameters:
+		// https://opencagedata.com/api#forward
+
+		var request = new XMLHttpRequest();
+		request.open("GET", request_url, true);
+
+		request.onload = function () {
+			// see full list of possible response codes:
+			// https://opencagedata.com/api#codes
+
+			if (request.status === 200) {
+				// Success!
+				var data = JSON.parse(request.responseText);
+				weather.fetchWeather(data.results[0].components.city);
+			} else if (request.status <= 500) {
+				// We reached our target server, but it returned an error
+
+				console.log(
+					"unable to geocode! Response code: " + request.status
+				);
+				var data = JSON.parse(request.responseText);
+				console.log("error msg: " + data.status.message);
+			} else {
+				console.log("server error");
+			}
+		};
+
+		request.onerror = function () {
+			// There was a connection error of some sort
+			console.log("unable to connect to server");
+		};
+
+		request.send(); // make the request
+	},
+	getLocation: function () {
+		function success(data) {
+			geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+		}
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(success, console.error);
+		} else {
+			weather.fetchWeather("Toronto");
+		}
+	},
+};
+
 document
 	.querySelector(".search-box button")
 	.addEventListener("click", function () {
@@ -54,4 +116,4 @@ document
 		}
 	});
 
-weather.fetchWeather("Toronto");
+geocode.getLocation();
